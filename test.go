cbde3eac9974139ec1c1e0be1d85ac000f2fc6f9
Alfred Eichenseher <alfred.eichenseher@googlemail.com>
2017-11-21 16:57:13 +0100

    adds root file recognition

--- test.go
+++ test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"io/ioutil"
 )
 
 type Audiobook struct {
@@ -31,6 +32,67 @@ func consume(interface{}) int {
 	return 0
 }
 
+func getBookName(path string) string {
+	//currentPath := filepath.Dir(path)
+	currentBook := filepath.ToSlash(path)
+	return currentBook[strings.LastIndex(currentBook, "/") + 1:]
+}
+
+func addFiles(book *Audiobook) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Print(err)
+			return nil
+		}
+		if strings.HasSuffix(info.Name(), ".mp3") {
+			fmt.Printf("Adding %s to %s \n", path, book.Name)
+			(*book).Files = append((*book).Files, path)
+		}
+		//fmt.Println(book)
+		return nil
+	}
+}
+
+func isNestedPath(audiobooks Audiobooks, path string) bool {
+	for k,_ := range audiobooks {
+		if strings.HasPrefix(path, k) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CreateAudioBook(audiobooks Audiobooks, path string) error {
+	bookName := getBookName(path)
+
+	if _, ok := audiobooks[path]; ok {
+		fmt.Printf("%v already in list\n", bookName)
+		return nil
+	} else {
+		// check if path is already used
+		if isNestedPath(audiobooks, path) {
+			fmt.Printf("%v already in list\n", bookName)
+			return nil
+		}
+		fmt.Printf("Add %v to list\n", bookName)
+		tmpbook := Audiobook{
+			Name: bookName,
+			Path: path,
+		}
+
+
+		err := filepath.Walk(path, addFiles(&tmpbook))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		audiobooks[path] = &tmpbook
+		//fmt.Println("New book...", tmpbook)
+	}
+	return nil
+}
+
 func scandir(audiobooks Audiobooks) filepath.WalkFunc {
 
 	return func(path string, info os.FileInfo, err error) error {
@@ -38,27 +100,25 @@ func scandir(audiobooks Audiobooks) filepath.WalkFunc {
 			log.Print(err)
 			return nil
 		}
-		if strings.HasSuffix(info.Name(), ".mp3") {
-			fmt.Printf("Found Mp3 %v in Folder %v \n", info.Name(), path)
-			//currentpath := path[0:strings.LastIndex(path, info.Name())]
-			currentpath := filepath.Dir(path)
-			currentbook := filepath.ToSlash(currentpath)
-			currentbook = currentbook[strings.LastIndex(currentbook, "/") + 1:]
 
-			// Check if we already have that book
-			if val, ok := audiobooks[currentpath]; ok {
-				val.Files = append(val.Files, info.Name())
-			} else {
-				tmpbook := Audiobook{
-					Name: currentbook,
-					Path: currentpath,
-
+		// Look inside if this might be a root folder
+		fmt.Println("Checking:",path)
+		if info.IsDir() {
+			files, err := ioutil.ReadDir(path)
+			if err != nil {log.Print(err); return nil; }
+			for _,v := range files {
+				if v.Name() == "ab_root" {
+					fmt.Println("Found a root file:", path)
+					CreateAudioBook(audiobooks,path)
+					return nil
 				}
-				tmpbook.Files = append(tmpbook.Files, info.Name())
-
-				audiobooks[currentpath] = &tmpbook
-				fmt.Println("New book...", tmpbook)
+				if strings.HasSuffix(v.Name(),"mp3") {
+					fmt.Println("Found a mp3 file:", path)
+					CreateAudioBook(audiobooks, path)
+					return nil
+				}
 			}
+
 		}
 		return nil
 	}
@@ -79,10 +139,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Results:")
+	fmt.Println("\n\nResults:")
 	for _, f := range books {
-		fmt.Println(f.Name);
-		fmt.Println(f.Path);
+		fmt.Println("Name: ",f.Name);
+		fmt.Println("Path: ", f.Path);
+		fmt.Println("Files: ")
 		for _, mp3 := range f.Files {
 			fmt.Print(mp3, ",")
 		}
